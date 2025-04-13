259c5eef977b0d0e3d56b6e30a6a50613d221258
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-04-13 20:29:32 +0200

    filter pills links

--- src/app/snippet/components/meta-data.tsx
+++ src/app/snippet/components/meta-data.tsx
@@ -1,14 +1,15 @@
 'use server';
 
 import Divider from "@/app/components/divider";
-import { Snippet } from "@prisma/client";
+import { Snippet, Tag } from "@prisma/client";
 import { Trash } from "lucide-react";
+import Link from "next/link";
 
 interface props {
     item: Snippet;
 }
 export default async function MetaData({ item }: props) {
-
+    console.log(item.tags)
     return (
         <div className="flex-1 p-4 flex flex-col gap-4">
             <h1 className="font-bold text-xl"> {item.title} </h1>
@@ -16,6 +17,14 @@ export default async function MetaData({ item }: props) {
             <Divider />
             <p> created: {item.created.toLocaleDateString()} - {item.created.toLocaleTimeString()} </p>
             <p> updated: {item.updated.toLocaleDateString()} - {item.updated.toLocaleTimeString()} </p>
+            <div className="flex gap-2">
+                {item.tags.map((tag: Tag, key: number) =>
+                    <Link
+                        href={`/?tag=${encodeURI(tag)}`}
+                        key={key}
+                        className="px-4 py-1 bg-stack rounded-full hover:bg-foreground hover:text-background"
+                    > #{tag} </Link>)}
+            </div>
             <Divider />
             <form
                 className="flex bg-stack rounded-lg p-2 gap-2"
