e10ed3294a2a6c10579ffe743d624ad90e31e2ee
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-04-13 20:28:56 +0200

    added filtering via tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,29 @@
 
 import database from "@/config/database";
 import Snippet from "./components/snippet";
+import { Tag } from "@prisma/client";
 
-export default async function page({ searchParams }: { searchParams: Promise<{ query: string }> }) {
+export default async function page({ searchParams }: { searchParams: Promise<{ query: string, tag: string }> }) {
+  const { query, tag } = await searchParams;
 
-  //construct database index-query from search
-  const query = (await searchParams).query;
-  const noQuery: boolean = !query || query.length < 0;
+  const filters: any = {};
+  if (query) {
+    filters.OR = [
+      { title: { contains: query, mode: 'insensitive' } },
+      { description: { contains: query, mode: 'insensitive' } },
+    ];
+  }
+  if (tag) {
+    filters.tags = { has: tag as Tag };
+  }
 
   const snippets = await database.snippet.findMany({
-    where: noQuery ? undefined : {
-      OR: [
-        { title: { contains: query, mode: 'insensitive' } },
-        { description: { contains: query, mode: 'insensitive' } },
-      ]
-    }
+    where: Object.keys(filters).length > 0 ? filters : undefined,
   });
 
   return (
     <div className="flex-1 content-start grid grid-cols-2 overflow-y-scroll gap-4">
-      {snippets.map((snippet, key: number) =>
+      {snippets.map((snippet, key) =>
         <Snippet key={key} snippet={snippet} />)}
     </div>
   );
