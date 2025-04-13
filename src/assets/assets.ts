01a5f778970e869dcdaa78be50ea7daf7740f163
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-04-13 20:28:06 +0200

    re-exporting tags array

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,7 +1,9 @@
 import banner from "@/assets/images/banner.png";
 import languages from "@/assets/json/languages.json";
+import tags from "@/assets/json/tags.json";
 
 export {
     banner,
     languages,
+    tags,
 }
