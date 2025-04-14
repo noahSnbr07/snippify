033417dab238db1e9f1e55dc32f1dc4b5675d6f6
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-04-14 14:48:49 +0200

    modified for more type safety

--- src/functions/get-prefix.ts
+++ src/functions/get-prefix.ts
@@ -8,6 +8,11 @@
  */
 export default function getPrefix(): URL {
 
-    const environment = process.env.NODE_ENV;
-    return new URL(environment === "development" ? "http://localhost:3000/" : "https://snippify-beta.vercel.app/");
+    try {
+        const environment = process.env.NODE_ENV;
+        return new URL(environment === "development" ? "http://localhost:3000/" : "https://snippify-beta.vercel.app/");
+    } catch (error) {
+        if (error instanceof Error) throw new Error(error.message)
+    }
+    return new URL("/error");
 }
