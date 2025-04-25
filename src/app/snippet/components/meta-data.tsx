7766c0e0df9a378f3571c20535ee488fd62ee545
noahSnbr07 <nur.noah.saschenbrecker@gmail.com>
2025-04-25 21:05:57 +0200

    global action button

--- src/app/snippet/components/meta-data.tsx
+++ src/app/snippet/components/meta-data.tsx
@@ -6,16 +6,17 @@ import { Tag } from "@prisma/client";
 import { ExternalLink, Trash } from "lucide-react";
 import Link from "next/link";
 import ShareButton from "./share-button";
+import ActionButton from "@/app/components/action-button";
 
 interface props {
     item: SnippetWithUser;
 }
+
 export default async function MetaData({ item }: props) {
 
-    const authentication = await getAuthentication();
-
     //check owner status
-    const isOwner = authentication?.id === item.user.id;
+    const authentication = await getAuthentication();
+    const isOwner = authentication && authentication.id === item.user.id;
 
     return (
         <div className="flex-1 px-4 flex flex-col gap-4">
@@ -40,22 +41,10 @@ export default async function MetaData({ item }: props) {
             <Divider />
             {isOwner && (
                 <>
-                    <form
-                        className="flex bg-stack rounded-lg p-2 gap-2"
-                        method="POST"
-                        action={endpoints(null, item.slug).snippet.post.delete}>
-                        <button
-                            type="submit"
-                            className="bg-stack rounded-lg p-2">
-                            <Trash />
-                        </button>
-                        <input
-                            required
-                            type="password"
-                            name="authorization"
-                            placeholder="your password"
-                            className="flex-1 px-4 py-1 bg-background rounded-md" />
-                    </form>
+                    <ActionButton
+                        icon={<Trash />}
+                        title="Delete Snippet"
+                        endpoint={endpoints(null, item.slug, true, null, null).snippet.post.delete} />
                     <Divider />
                 </>
             )}
